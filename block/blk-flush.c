b6b548708aa0631cccdafaa1fa433f8424901450
flar2 <asegaert@gmail.com>
2014-04-06 13:00:09 -0400

    Revert "block: account iowait time when waiting for completion of IO request"
    This reverts commit 1531b5caf879bbb3cc1d22c459d4e8125b11649b.

--- block/blk-flush.c
+++ block/blk-flush.c
@@ -307,7 +307,7 @@ int blkdev_issue_flush(struct block_device *bdev, gfp_t gfp_mask,
 
 	bio_get(bio);
 	submit_bio(WRITE_FLUSH, bio);
-	wait_for_completion_io(&wait);
+	wait_for_completion(&wait);
 
 	if (error_sector)
                *error_sector = bio->bi_sector;
